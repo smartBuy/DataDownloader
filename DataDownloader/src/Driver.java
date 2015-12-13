a19023cf874c7981cc6286848f8e8c932f14cdb8
smartBuy <smart.buy.proj@gmail.com>
2015-12-13 19:19:36 +0200

    adding driver

---
+++ DataDownloader/src/Driver.java
@@ -0,0 +1,17 @@
+
+import java.net.*;
+public class Driver {
+
+	public static void main(String[] args) {
+		try {
+			URL myURL = new URL("http://example.com/");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
