3d1e6dd45a82534f78be7a08e1a3dbfdb955458a
Dima <dimagb@hotmail.co.uk>
2015-12-15 14:41:17 +0200

    t

--- DataDownloader/src/Driver.java
+++ DataDownloader/src/Driver.java
@@ -5,10 +5,13 @@ public class Driver {
 	public static void main(String[] args) {
 		try {
 			URL myURL = new URL("http://example.com/");
+			System.out.println(myURL);
+
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 		
 
